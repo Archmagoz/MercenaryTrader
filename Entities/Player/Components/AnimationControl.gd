741a4cbb1f26d71c161296fee135fd357a0cd06f
Archmagoz <0josegabrielmatos0@gmail.com>
2025-12-06 06:53:14 -0300

    first commit

---
+++ Entities/Player/Components/AnimationControl.gd
@@ -0,0 +1,55 @@
+extends Node2D
+
+class_name AnimationControl
+
+@onready var body  := $"../Body"
+@onready var hair  := $"../Hair"
+
+var current_animation := "none"
+var current_direction := 1.0
+
+func _ready() -> void:
+	set_animation("idle")
+
+func set_animation(new_animation: String) -> void:
+	if new_animation == current_animation:
+		return
+
+	current_animation = new_animation
+	body.play(new_animation)
+	hair.play(new_animation)
+
+func set_direction(new_direction: float) -> void:
+	if new_direction == current_direction:
+		return
+
+	current_direction = new_direction
+	body.scale.x = new_direction
+	hair.scale.x = new_direction
+
+func idle() -> void:
+	set_animation("idle")
+
+func walk(direction: float) -> void:
+	set_direction(direction)
+	set_animation("walk")
+
+func run(direction: float) -> void:
+	set_direction(direction)
+	set_animation("run")
+
+func jump() -> void:
+	set_animation("jump")
+
+func handle_animation(animation: String, direction: float, is_on_floor: bool) -> void:
+	if not is_on_floor:
+		jump()
+		return
+
+	match animation:
+		"idle":
+			idle()
+		"walk":
+			walk(direction)
+		"run":
+			run(direction)
---
+++ Entities/Player/Components/Player.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -400.0
+
+@onready var animation := $AnimationControl
+
+func _physics_process(delta: float) -> void:
+	_handle_movement(delta)
+
+func _handle_movement(delta: float) -> void:
+	var direction: float = Input.get_axis("ui_left", "ui_right")
+	var on_floor: bool = is_on_floor()
+	var target_animation: String
+
+	if not on_floor:
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("ui_accept") and on_floor:
+		velocity.y = JUMP_VELOCITY
+		target_animation = "jump"
+
+	if direction != 0:
+		velocity.x = direction * SPEED
+		target_animation = "walk"
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		target_animation = "idle"
+
+	animation.handle_animation(target_animation, direction, on_floor)
+	move_and_slide()
